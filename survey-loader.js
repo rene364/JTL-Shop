4134f927a98ce0465349fbcd6587b351084585b8
rene364 <rene@neotec.gmbh>
2026-04-14 17:17:10 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -3,9 +3,6 @@ function loadGoogleForm() {
     if (target) {
         // Variante B (Sicherer für alle Browser): Das CSS-Display nutzen
         target.style.display = 'block';
-         // Den Fokus aktiv vom iFrame weglenken
-        target.setAttribute('tabindex', '-1'); // Macht das Div fokussierbar
-        target.focus({ preventScroll: true }); // Setzt Fokus, verbietet aber das Scrollen
     }    
 }
 
