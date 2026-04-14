5a9a93c6c4760702141ed12a4cca5d647348509b
rene364 <rene@neotec.gmbh>
2026-04-14 13:52:55 +0200

    Create survey-loader.js

---
+++ survey-loader.js
@@ -0,0 +1,12 @@
+function loadGoogleForm() {
+    var googleUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdr7mTb-_EiZpe48nO7ufwp35CRXXAXarkOQ2V5RGJm_Gs1tQ/viewform?embedded=true";
+    var target = document.getElementById('survey-target');
+    var box = document.getElementById('survey-box');
+    
+    if (target && box) {
+        target.innerHTML = '<iframe src="' + googleUrl + '" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0" title="Google Form Umfrage">Lädt...</iframe>';
+        box.style.display = 'none';
+        // Optional: Scrollt zum Formular, falls die Seite lang ist
+        target.scrollIntoView({ behavior: 'smooth' });
+    }
+}
