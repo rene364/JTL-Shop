b07adcba52e56e5750307ae57f2fa8f237234606
rene364 <rene@neotec.gmbh>
2026-04-14 14:45:21 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -1,41 +1,12 @@
 function loadGoogleForm() {
-    var googleUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdr7mTb-_EiZpe48nO7ufwp35CRXXAXarkOQ2V5RGJm_Gs1tQ/viewform";
+    var googleUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdr7mTb-_EiZpe48nO7ufwp35CRXXAXarkOQ2V5RGJm_Gs1tQ/viewform?embedded=true";
     var target = document.getElementById('survey-target');
     var box = document.getElementById('survey-box');
     
     if (target && box) {
-        // 1. Box ausblenden
+        target.innerHTML = '<iframe src="' + googleUrl + '" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0" title="Google Form Umfrage">Lädt...</iframe>';
         box.style.display = 'none';
-
-        // 2. Iframe dynamisch erstellen
-        var iframe = document.createElement('iframe');
-        iframe.src = googleUrl;
-        iframe.width = "100%";
-        iframe.height = "1000";
-        iframe.style.border = "none";
-        iframe.style.opacity = "0"; // Erst unsichtbar, um Flackern zu vermeiden
-        iframe.style.transition = "opacity 0.5s";
-        
-        // 3. PRÜFUNG: Wann ist das Iframe fertig?
-        iframe.onload = function() {
-            // Iframe sichtbar machen
-            iframe.style.opacity = "1";
-            
-            // Verzögerung, um den automatischen Google-Fokus abzufangen
-            setTimeout(function() {
-                // Position des Targets berechnen
-                // Wir ziehen 150px ab, damit der Nova-Header nicht drüber liegt
-                const yOffset = -150; 
-                const y = target.getBoundingClientRect().top + window.pageYOffset + yOffset;
-
-                window.scrollTo({top: y, behavior: 'smooth'});
-
-                // Fokus auf das Target setzen, um Google den Fokus zu "entziehen"
-                target.setAttribute('tabindex', '-1');
-                target.focus({preventScroll: true});
-            }, 3000); // 200ms warten sicherheitshalber
-        };
-
-        target.appendChild(iframe);
+        // Optional: Scrollt zum Formular, falls die Seite lang ist
+        target.scrollIntoView({ behavior: 'smooth' });
     }
 }
