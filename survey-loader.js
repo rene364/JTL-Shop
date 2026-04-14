4f76ab38aba36663e4ef009c4540877e14502312
rene364 <rene@neotec.gmbh>
2026-04-14 17:12:59 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -9,6 +9,13 @@ function loadGoogleForm() {
          // Den Fokus aktiv vom iFrame weglenken
         container.setAttribute('tabindex', '-1'); // Macht das Div fokussierbar
         container.focus({ preventScroll: true }); // Setzt Fokus, verbietet aber das Scrollen
+
+        iframe.onload = function() {
+    setTimeout(function() {
+        window.scrollTo(200, 0); // Zwingt den Browser nach ganz oben
+        document.activeElement.blur(); // Entzieht dem gerade aktiven Element (iFrame) den Fokus
+    }, 50); 
+};
     
     }
 }
