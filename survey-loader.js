a7a10b0c250a54f7da1ca19290573c1172fdce1b
rene364 <rene@neotec.gmbh>
2026-04-14 17:46:52 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -7,15 +7,8 @@ function loadGoogleForm() {
         box.style.display = 'none';
         wrapper.style.display = 'block';
 
-        // 2. Den JTL "Go-to-top" Button suchen und triggern
-        var topButton = document.querySelector('#footer > div.smoothscroll-top.go-to-top.scroll-to-top show');
-        
-        if (topButton) {
-            topButton.click(); // Simuliert den Klick des Nutzers
-        } else {
-            // Fallback, falls der Button nicht gefunden wird
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
+        // Fallback, falls der Button nicht gefunden wird
+        window.scrollTo({ top: 0, behavior: 'smooth' });
 
         // 3. Fokus abkoppeln, damit Google nicht zurückspringt
         wrapper.setAttribute('tabindex', '-1');
