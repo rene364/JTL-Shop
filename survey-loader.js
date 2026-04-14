ec207ddb64e5ad9ce4697fbcb8e5fc3c8e1d1488
rene364 <rene@neotec.gmbh>
2026-04-14 17:24:51 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -1,5 +1,6 @@
 function loadGoogleForm() {
     // iFrame mit id
+    var target = document.getElementById('umfrage');
     if (target) {
         // Variante B (Sicherer für alle Browser): Das CSS-Display nutzen
         target.style.display = 'block';
