ccc79000d6f2394087d415ec1e576d364adb07c8
rene364 <rene@neotec.gmbh>
2026-04-15 09:08:46 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -3,6 +3,6 @@ function loadGoogleForm() {
     var googleUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdr7mTb-_EiZpe48nO7ufwp35CRXXAXarkOQ2V5RGJm_Gs1tQ/viewform?embedded=true"; 
     var target = document.getElementById('survey-target'); 
     var box = document.getElementById('survey-box'); 
-    target.innerHTML = '<iframe src="' + googleUrl + '" width="640" height="800" frameborder="0" marginheight="100" marginwidth="0">Lädt...</iframe>'; 
+    target.innerHTML = '<iframe src="' + googleUrl + '" width="640" height="654" frameborder="0" marginheight="100" marginwidth="0">Lädt...</iframe>'; 
     box.style.display = 'none'; // Blendet den Hinweistext nach Klick aus
 }
