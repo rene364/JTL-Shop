9c4869a92d718f6d3f927bdfdab07a21fb0f3d88
rene364 <rene@neotec.gmbh>
2026-04-15 08:44:14 +0200

    Update survey-loader-2.js

--- survey-loader-2.js
+++ survey-loader-2.js
@@ -5,3 +5,4 @@ function loadGoogleForm() {
     var box = document.getElementById('survey-box');
     target.innerHTML = '<iframe src="' + googleUrl + '" width="100%" height="800" frameborder="0" marginheight="0" marginwidth="0">Lädt...</iframe>';
     box.style.display = 'none'; // Blendet den Hinweistext nach Klick aus
+}
