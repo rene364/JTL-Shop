8de80b8f50f44f63d48fa5a5f46a863b9e8ae621
rene364 <rene@neotec.gmbh>
2026-04-15 09:25:01 +0200

    Update survey-loader.js

--- survey-loader.js
+++ survey-loader.js
@@ -1,15 +1,21 @@
 function loadGoogleForm() { 
-    // ERSETZE DEN LINK UNTEN DURCH DEINEN EIGENEN GOOGLE-LINK 
     var googleUrl = "https://docs.google.com/forms/d/e/1FAIpQLSdr7mTb-_EiZpe48nO7ufwp35CRXXAXarkOQ2V5RGJm_Gs1tQ/viewform?embedded=true"; 
     var target = document.getElementById('survey-target'); 
     var box = document.getElementById('survey-box'); 
-    target.innerHTML = '<iframe src="' + googleUrl + '" width="640" height="654" frameborder="0" marginheight="100" marginwidth="0">Lädt...</iframe>'; 
-    box.style.display = 'none'; // Blendet den Hinweistext nach Klick aus
     
-    // Fallback, falls der Button nicht gefunden wird
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    // 1. Inhalt setzen und Box ausblenden
+    // Tipp: width="100%" macht es mobil-freundlicher
+    target.innerHTML = '<iframe src="' + googleUrl + '" width="100%" height="654" frameborder="0" marginheight="0" marginwidth="0">Lädt...</iframe>'; 
+    box.style.display = 'none';
 
-    // 3. Fokus abkoppeln, damit Google nicht zurückspringt
-    target.setAttribute('tabindex', '-1');
-    target.focus({ preventScroll: true });
+    // 2. Fokus-Abkopplung mit minimaler Verzögerung
+    // Wir warten 50ms, damit der Browser das iFrame registriert hat,
+    // und "stehlen" dann den Fokus.
+    setTimeout(function() {
+        target.setAttribute('tabindex', '-1');
+        target.focus({ preventScroll: true });
+        
+        // 3. Scroll nach ganz oben (berücksichtigt den JTL-Header)
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }, 50);
 }
